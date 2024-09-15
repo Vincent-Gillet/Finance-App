19695889daccd2eddf7320315c2bfa9889e11a2e
Vincent-Gillet <vincent.gillet.vg@gmail.com>
2024-09-15 21:09:09 +0200

    Delete route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,24 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-// Route::get('/api/documentation', function () {
-//     return view('vendor.l5-swagger.index', [
-//         'documentation' => 'default',
-//         'urlToDocs' => url('api-docs.json'),
-//         'operationsSorter' => null,
-//         'configUrl' => null,
-//         'validatorUrl' => null,
-//         'useAbsolutePath' => true,
-//     ]);
-// });
-
-// Route::get('api/documentation', function () {
-//     return view('swagger.index');
-// });
-
-// Route::get('api/documentation', [SwaggerController::class, 'api']);
-
 Route::get('/api-docs.json', function () {
     return response()->file(storage_path('api-docs/swagger.json'));
 });
